258315452e81f45cadad1832ea2c2930dabc2e06
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-06-06 16:15:43 +0300

    add function to do square of a number

---
+++ FUNCTIONS/find_square_of_number.py
@@ -0,0 +1,7 @@
+num1=input("Enter a number to square: ")
+
+def  square_of_number(num1):
+    square= int(num1) ** 2
+    return square
+result=square_of_number(num1)
+print("The square of the number is:", result)
